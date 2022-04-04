b2b0cab0627181c69ece092de1239c48089125ab
NickSettler <moiseevnikita14@gmail.com>
2022-04-05 00:27:07 +0200

    adds weather data cards

--- src/components/common/WeatherData/WeatherData.styled.ts
+++ src/components/common/WeatherData/WeatherData.styled.ts
@@ -1,9 +1,13 @@
-import { Breadcrumbs, Stack, styled } from "@mui/material";
+import { Box, Breadcrumbs, Stack, styled } from "@mui/material";
 
 export const WeatherDataContainer = styled(Stack)(({ theme }) => ({
   marginTop: theme.spacing(4),
 }));
 
 export const WeatherDataBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
+  marginBottom: theme.spacing(3),
+}));
+
+export const WeatherDataInfoSection = styled(Box)(({ theme }) => ({
   marginBottom: theme.spacing(2),
 }));
--- src/components/common/WeatherData/WeatherData.tsx
+++ src/components/common/WeatherData/WeatherData.tsx
@@ -12,15 +12,21 @@ import {
 import {
   Avatar,
   Card,
+  CardContent,
   CardHeader,
   CircularProgress,
   Grid,
+  Stack,
   Typography,
 } from "@mui/material";
 import {
   WeatherDataBreadcrumbs,
   WeatherDataContainer,
+  WeatherDataInfoSection,
 } from "./WeatherData.styled";
+import { Air, CloudQueue, Opacity, Thermostat } from "@mui/icons-material";
+import { windDirection } from "../../../helpers/api";
+import moment from "moment-timezone";
 
 const WeatherData = (props: WeatherDataProps): JSX.Element => {
   const { currentWeatherItem, isLoading } = props;
@@ -42,26 +48,97 @@ const WeatherData = (props: WeatherDataProps): JSX.Element => {
           {currentWeatherItem.title}
         </Typography>
       </WeatherDataBreadcrumbs>
+      <WeatherDataInfoSection>
+        <Grid container spacing={3}>
+          <Grid item xs={6} sm={3}>
+            <Stack direction={"column"} spacing={1} alignItems={"flex-start"}>
+              <Stack direction={"row"} spacing={1} alignItems={"baseline"}>
+                <Typography variant={"subtitle2"}>Sunrise:</Typography>
+                <Typography variant={"body1"}>
+                  {moment(currentWeatherItem.sun_rise)
+                    .tz(currentWeatherItem.timezone)
+                    .format("HH:mm")}
+                </Typography>
+              </Stack>
+              <Stack direction={"row"} spacing={1} alignItems={"baseline"}>
+                <Typography variant={"subtitle2"}>Sunset:</Typography>
+                <Typography variant={"body1"}>
+                  {moment(currentWeatherItem.sun_set)
+                    .tz(currentWeatherItem.timezone)
+                    .format("HH:mm")}
+                </Typography>
+              </Stack>
+            </Stack>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Stack direction={"row"} spacing={1} alignItems={"baseline"}>
+              <Typography variant={"subtitle2"}>Current time:</Typography>
+              <Typography variant={"body1"}>
+                {moment(currentWeatherItem.time)
+                  .tz(currentWeatherItem.timezone)
+                  .format("HH:mm")}
+              </Typography>
+            </Stack>
+            <Stack direction={"row"} spacing={1} alignItems={"baseline"}>
+              <Typography variant={"subtitle2"}>Timezone:</Typography>
+              <Typography variant={"body1"}>
+                {currentWeatherItem.timezone.replace("_", " ")}
+              </Typography>
+            </Stack>
+          </Grid>
+        </Grid>
+      </WeatherDataInfoSection>
       <Grid container spacing={4}>
         {currentWeatherItem.consolidated_weather.map(
-          (weather: WeatherDataType) => (
-            <Grid xs={12} sm={6} md={4} lg={3} item key={weather.id}>
-              <Card>
-                <CardHeader
-                  avatar={
-                    <Avatar
-                      alt={weather.weather_state_name}
-                      src={WeatherApi.getIconUrl(weather.weather_state_abbr)}
-                    />
-                  }
-                  title={weather.weather_state_name}
-                  subheader={`
-                    ${Math.round(weather.min_temp)}°C - 
-                    ${Math.round(weather.max_temp)}°C`}
-                />
-              </Card>
-            </Grid>
-          )
+          (weather: WeatherDataType) => {
+            const date = moment(weather.applicable_date)
+              .tz(currentWeatherItem.timezone)
+              .format("dddd, MMMM Do YYYY");
+            return (
+              <Grid xs={12} sm={6} md={4} lg={3} item key={weather.id}>
+                <Card>
+                  <CardHeader
+                    avatar={
+                      <Avatar
+                        alt={weather.weather_state_name}
+                        src={WeatherApi.getIconUrl(weather.weather_state_abbr)}
+                      />
+                    }
+                    title={date}
+                    subheader={weather.weather_state_name}
+                  />
+                  <CardContent>
+                    <Stack direction={"row"} spacing={1} alignItems={"center"}>
+                      <Thermostat color={"info"} titleAccess={"Temperature"} />
+                      <Typography variant={"body1"}>
+                        {`${Math.round(weather.min_temp)}°C - 
+                          ${Math.round(weather.max_temp)}°C`}
+                      </Typography>
+                    </Stack>
+                    <Stack direction={"row"} spacing={1} alignItems={"center"}>
+                      <Air color={"info"} titleAccess={"Wind"} />
+                      <Typography variant={"body1"}>
+                        {Math.round(weather.wind_speed)} km/h{" "}
+                        {windDirection(weather.wind_direction_compass)}
+                      </Typography>
+                    </Stack>
+                    <Stack direction={"row"} spacing={1} alignItems={"center"}>
+                      <CloudQueue color={"info"} titleAccess={"Pressure"} />
+                      <Typography variant={"body1"}>
+                        {Math.round(weather.air_pressure)} hPa
+                      </Typography>
+                    </Stack>
+                    <Stack direction={"row"} spacing={1} alignItems={"center"}>
+                      <Opacity color={"info"} titleAccess={"Humidity"} />
+                      <Typography variant={"body1"}>
+                        {Math.round(weather.humidity)} %
+                      </Typography>
+                    </Stack>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          }
         )}
       </Grid>
     </WeatherDataContainer>
